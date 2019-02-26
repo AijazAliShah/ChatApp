e7e8a965768920231b7c2af43738a4864126266a
aijaz ali <you@example.com>
2019-02-26 16:19:30 +0500

    complete

--- src/containers/ChatWindow.js
+++ src/containers/ChatWindow.js
@@ -6,16 +6,20 @@ import _ from "lodash";
 
 import "./ChatWindow.css"
 
+import MessageInput from "./MessageInput";
+
 const ChatWindow = ({ activeUserId }) => {
   const state = store.getState();
   const activeUser = state.contacts[activeUserId];
   const activeMsgs = state.messages[activeUserId];
+  const { typing } = state;
 
 
   return (
     <div className="ChatWindow">
       <Header user={activeUser} />
       <Chats messages={_.values(activeMsgs)} />
+      <MessageInput value={typing} />
     </div>
   );
 };
